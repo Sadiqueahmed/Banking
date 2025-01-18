9219f4881431272e52b8e39eb1a0626b9d1a0b0e
Sadique Ahmed <sadiqueahmed40@gmail.com>
2025-01-18 15:41:07 +0530

    Create CustomersRequestDTO.java

---
+++ Banking/src/main/java/com/example/Banking/DTO/CustomersRequestDTO.java
@@ -0,0 +1,70 @@
+package com.example.Banking.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+public class CustomersRequestDTO {
+
+    @JsonProperty("name")
+    private String customerName;
+
+    @JsonProperty("address")
+    private String customerAddress;
+
+    @JsonProperty("number")
+    private String phoneNumber;
+
+    @JsonProperty("email")
+    private String customerEmail;
+
+    @JsonProperty("password")
+    private String password;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
+
